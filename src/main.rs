299b44dbda90fdfb533815e1bb905feeb08ab689
Dave Huseby <github@xf.rs>
2019-01-06 14:48:11 -0800

    added simple phrases db file

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,10 @@
 #[macro_use] extern crate rocket;
 use rocket::response::NamedFile;
 use rocket::State;
-use std::path::Path;
 use std::collections::HashMap;
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+use std::path::Path;
 use std::sync::Arc;
 
 #[get("/favicon.ico")]
@@ -29,10 +31,15 @@ type Phrases = HashMap<String, String>;
 
 fn main() {
     let mut db = Phrases::new();
-    db.insert(
-        "good morning".to_string(),
-        "доброе утро".to_string()
-    );
+
+    let file = BufReader::new(File::open(Path::new("static/phrases.txt")).unwrap());
+    file.lines().for_each(|l| {
+        let line = l.unwrap();
+        let kvp: Vec<_> = line.split(",").collect();
+        if kvp.len() < 2 { return; }
+        println!("Loading {} = {}", kvp[0], kvp[1]);
+        db.insert(kvp[0].to_string(), kvp[1].to_string());
+    });
     rocket::ignite()
         .manage(Arc::new(db))
         .mount("/", routes![russian, favicon, index])
